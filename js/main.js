7ce3d60806a9e66a04d5e88e52c7aed50d25c941
Joyce Van Herck <joyce.vherck@gmail.com>
2015-09-01 21:09:58 +0100

    jsonp call

--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@ $(function() {
 
 		init: function() {
 
-			
+			$.ajax({
+			    url: "https://api.github.com/users/nasa/repos?callback=replace_me&format=jsonp",
+			    jsonp: "callback",
+			    dataType: "jsonp",
+			    success: function( response ) {
+			        console.log( response.data ); // server response
+			    },
+			    error: function(e) {
+			       console.log(e.message);
+			    }
+			});
 
 		}
 

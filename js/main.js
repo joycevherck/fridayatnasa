5e74a5dd753974bcc079b0c2b6b0dcf27428713e
Joyce Van Herck <joyce.vherck@gmail.com>
2015-09-01 21:25:38 +0100

    get programming languages from data

--- js/main.js
+++ js/main.js
@@ -9,7 +9,15 @@ $(function() {
 			    jsonp: "callback",
 			    dataType: "jsonp",
 			    success: function( response ) {
-			        console.log( response.data ); // server response
+			        var data = response.data;
+			        var results = [];
+
+			        $.each(data, function(i, val) {
+			        	results.push(val.language);
+			        });
+
+			        results.sort();
+			        console.log(results);
 			    },
 			    error: function(e) {
 			       console.log(e.message);
